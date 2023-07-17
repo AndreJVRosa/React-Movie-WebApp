89ddc1f4c4563955723aed77a8c2d1f898fdd992
AndreJVRosa <130563935+AndreJVRosa@users.noreply.github.com>
2023-07-17 20:46:41 +0100

    Lamp Test Failed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
+import useState from 'react'
+import styled  from 'styled-components'
+import Lamp from './Lamp.jsx';
+import LightSwitch from './LightSwitch.jsx';
+
+const Room = styled.div`
+  position: relative;
+  width: 500px;
+  height: 500px;
+  border: 10px solid black;
+  margin: 0 auto;
+`;
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLampOneOn, setIsLampOneOn] = useState(false);
+  const [isLampTwoOn, setIsLampTwoOn] = useState(false);
+
+  const handleLightSwitchOne = () => setIsLampOneOn(prev => !prev);
+  const handleLightSwitchTwo = () => setIsLampTwoOn(prev => !prev);
 
   return (
-    <>
-      <div className='App'>
-        This is a little Star
-        </div>
-    </>
+    <Room>
+      <Lamp lampOn = {isLampOneOn} position = 'left'/>
+      <Lamp lampOn = {isLampTwoOn} position = 'right'/>
+      <LightSwitch
+      name = 'one'
+      callback = {handleLightSwitchOne}
+      switchOn = {isLampOneOn}
+      position = 'left'
+      />
+      <LightSwitch
+      name = 'two'
+      callback = {handleLightSwitchTwo}
+      switchOn = {isLampTwoOn}
+      position = 'right'
+      />
+    </Room>
   )
 }
 
