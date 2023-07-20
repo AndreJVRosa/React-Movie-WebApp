fdeebee593f553ac4e859ba9ece795188ea18442
André Rosa <andre.rosa012@gmail.com>
2023-07-20 20:29:52 +0100

    Home doen's work and nither does header now great

---
+++ src/componentes/Home.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+//config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
+
+//Components
+
+//Hook
+
+//Image
+import NoImage from '../assets/no_image.jpg'
+
+
+const Home = () => {
+   const [state, setState] = useState();
+   const [loading, setLoading] = useState(false);
+   const [error, setError] = useState(false);
+
+    return <div>Home Page</div>
+}
+
+export default Home
